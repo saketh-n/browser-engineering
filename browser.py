56d87fc9c6f559ce0f39e088d614620a0bf96c44
Saketh <sakethn@cs.washington.edu>
2026-04-29 11:46:20 -0700

    Completed http exercises up to 6, 6 In Progress

---
+++ browser.py
@@ -0,0 +1,134 @@
+import socket
+import ssl
+import certifi
+import os
+
+class URL:
+    def __init__(self, url):
+        # Special Case, handling data urls
+        dataScheme = "data:text/html"
+        if url.startswith(dataScheme):
+            self.scheme = "data"
+            _, self.path = url.split(",", 1)
+            return
+        # Handling View Source
+        viewSourceString = "view-source:"
+        self.viewSource = False
+        if url.startswith("view-source:"):
+            _, url = url.split(viewSourceString, 1)
+            self.viewSource = True
+
+        # Extract the relevant pieces from the url step by step
+        # <scheme://hostname/path>
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https", "file"]
+
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
+    def internet_request(self, headers=[]):
+        # using the given url, make an http request to it
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        s.connect((self.host, self.port))
+        if self.scheme == "https":
+            ctx = ssl.create_default_context(cafile=certifi.where())
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
+        # Every piece of this is relevant to the HTTP format
+        request = "GET {} HTTP/1.1\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        # Necessary in 1.1, otherwise it's keep alive and browser hangs waiting 
+        # for further responses
+        request += "Connection: keep-alive\r\n"
+        request += "User-Agent: python-browser\r\n"
+        for header in headers:
+            request += "{}\r\n".format(header)
+        request += "\r\n"
+
+        # turn the string into bytes, request needs to be in bytes
+        s.send(request.encode("utf8"))
+
+        # Turn the response into a file-like object
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+        # Decompose response
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n": break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        content = response.read(int(response_headers["content-length"]))
+        s.close()
+
+        if (self.viewSource):
+            content = "view-source:" + content
+
+        return content
+
+    def request(self, headers=[]):
+        if ("http" in self.scheme):
+            return self.internet_request()
+        
+        # File Request
+        if (self.scheme == "file"):
+            f = open(self.path)
+            return f.read()
+
+        # Data Request
+        if (self.scheme == "data"):
+            return self.path
+        
+
+def show(body):
+    in_tag = False
+    if (body.startswith("view-source:")):
+        print(body)
+        return
+    output = ""
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            output += c
+    output = output.replace("&lt;", "<")
+    output = output.replace("&gt;", ">")
+    print(output)
+        
+def load(url):
+    body = url.request()
+    show(body)    
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        load(URL(sys.argv[1]))
+    else:
+        load(URL("file:///Users/saketh/Documents/CS/browser-eng/code/welcome.txt"))
+    
+
