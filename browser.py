037af5a65d34e61abcb5c2eb31fbc4885c9f1364
Saketh <sakethn@cs.washington.edu>
2026-05-05 08:02:31 -0700

    Completed Ex 8 on html, added support for relative paths in file mode

--- browser.py
+++ browser.py
@@ -2,11 +2,14 @@ import socket
 import ssl
 import certifi
 import os
+import time
+
+sockets = {}
+cache = {}
 
 class URL:
     def __init__(self, url):
         self.parse_url(url)
-        self.sockets = {}
 
     def parse_url(self, url):
         # Special Case, handling data urls
@@ -32,27 +35,44 @@ class URL:
         elif self.scheme == "https":
             self.port = 443
 
-        if "/" not in url:
-            url = url + "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
+        if self.scheme != "file": 
+            if "/" not in url:
+                url = url + "/"
+            self.host, url = url.split("/", 1)
+            self.path = "/" + url
+            if ":" in self.host:
+                self.host, port = self.host.split(":", 1)
+                self.port = int(port)
+        else:
+            self.path = url
 
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+
+    def cache_content(self, content, response_headers, url):
+        cache_control_params = list(map(str.strip, response_headers["cache-control"].split(",")))
+        # any header other than max_age, don't
+        if len(cache_control_params) == 1:
+            if list(cache_control_params)[0].startswith("max-age"):
+                max_age = int(list(map(str.strip, list(cache_control_params)[0].split("=", 1)))[1])
+                cache[url] = {"content": content, "max_age": max_age, "time": time.time()}
 
     def internet_request(self, headers=[], redirect_limit=4):
         # using the given url, make an http request to it
 
         # First check that a cached socket exists
-        if self.host in self.sockets:
-            s = self.sockets[self.host]
+        if self.host in sockets:
+            s = sockets[self.host]
         else:
             s = socket.socket(
                 family=socket.AF_INET,
                 type=socket.SOCK_STREAM,
                 proto=socket.IPPROTO_TCP,
             )
+
+        url = self.host + self.path
+        if url in cache:
+            # Check timestamp against max_age. Make sure it didn't expire
+            if (time.time() <= cache[url]["time"] + cache[url]["max_age"]):
+                return cache[url]["content"]
 
         # Every piece of this is relevant to the HTTP format
         request = "GET {} HTTP/1.1\r\n".format(self.path)
@@ -100,21 +120,30 @@ class URL:
             # redirect request
             # get location header
             redirect_url = response_headers["location"]
-            # check if it's justa local redirect
+            # check if it's just a local redirect
             if redirect_url.startswith("/"):
                 self.path = redirect_url
             else:
                 self.parse_url(redirect_url)
-            return self.internet_request(redirect_limit = redirect_limit - 1)
+            content = self.internet_request(redirect_limit = redirect_limit - 1)
+            if "cache-control" in response_headers and status == "301":
+                # Cache
+                self.cache_content(content, response_headers, url)
+                
+            return content
 
         assert "transfer-encoding" not in response_headers
         assert "content-encoding" not in response_headers
 
         content = response.read(int(response_headers["content-length"])).decode("utf8")
-        self.sockets[self.host] = s
+        sockets[self.host] = s
 
         if (self.viewSource):
             content = "view-source:" + content
+
+        # Should we cache
+        if "cache-control" in response_headers and (status == '200' or status == '404'):
+            self.cache_content(content, response_headers, url)
 
         return content
 
@@ -159,6 +188,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         load(URL(sys.argv[1]))
     else:
-        load(URL("file:///Users/saketh/Documents/CS/browser-eng/code/welcome.txt"))
+        load(URL("file://./welcome.txt"))
     
 
